4d8b831b62215fca70fba385d2f5e2e7c7f0dd33
Kabir Deula <kabirdeula167@gmail.com>
2023-07-16 09:40:32 +0545

    fixed Today's Details

---
+++ app/src/main/java/com/mobileprogramming/incomeexpensestracker/DateWiseReport.java
@@ -0,0 +1,11 @@
+package com.mobileprogramming.incomeexpensestracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DateWiseReport extends Activity {
+    @Override
+    protected void onCreate(Bundle viewSavedInstance){
+        super.onCreate(viewSavedInstance);
+    }
+}
--- app/src/main/java/com/mobileprogramming/incomeexpensestracker/TodayDetails.java
+++ app/src/main/java/com/mobileprogramming/incomeexpensestracker/TodayDetails.java
@@ -1,4 +1,50 @@
 package com.mobileprogramming.incomeexpensestracker;
 
-public class TodayDetails {
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TodayDetails extends Activity {
+    RecyclerView rv;
+    RecyclerView.Adapter recyclerAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    MyDatabase myDatabase;
+    Cursor cursor;
+
+    @Override
+    protected void onCreate(Bundle viewSavedInstance){
+        super.onCreate(viewSavedInstance);
+        setContentView(R.layout.activity_today_details);
+        rv = findViewById(R.id.rv);
+        myDatabase = new MyDatabase(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        loadData();
+    }
+
+    public void loadData(){
+        cursor = myDatabase.selectData(MainActivity.getCurrentDate());
+        ArrayList<DataModel> data = new ArrayList<>();
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(0);
+            String date = cursor.getString(1);
+            int income = cursor.getInt(2);
+            int expenses = cursor.getInt(3);
+            DataModel dataModel = new DataModel(id, date, income, expenses);
+            data.add(dataModel);
+        }
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerAdapter = new RecyclerViewAdapter(this, data);
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(recyclerAdapter);
+    }
 }
